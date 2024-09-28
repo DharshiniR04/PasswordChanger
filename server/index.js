1aa9ec19e5dc743ff34fe18bba48f7fee294bfda
Dharshini R <114658091+DharshiniR04@users.noreply.github.com>
2024-09-28 10:38:34 +0530

    Updated index.js on CORS

--- server/index.js
+++ server/index.js
@@ -10,18 +10,18 @@ dotenv.config();
 connectDB();
 
 const app = express();
-// app.use(cors({
-//     origin: "https://safe-hold-client.vercel.app", 
-//     methods: ["POST", "GET","PATCH"],
-//     credentials: true
-//   }));
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "https://safe-hold-client.vercel.app"); 
-//     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     next();
-//   });
+app.use(cors({
+    origin: "https://password-changer-client.vercel.app/", 
+    methods: ["POST", "GET","PATCH"],
+    credentials: true
+  }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://password-changer-client.vercel.app/"); 
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
 app.use(cors());
 app.use(express.json());
 
